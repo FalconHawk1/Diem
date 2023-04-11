0065c2ac17c87a4f87f262b31f46ab8c6bac9295
FalconHawk1 <fabian.alarconvega7@gmail.com>
2023-04-10 21:50:27 -0500

    choose login & register

--- app/src/main/java/com/example/diem/MainActivity.java
+++ app/src/main/java/com/example/diem/MainActivity.java
@@ -3,9 +3,6 @@ package com.example.diem;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                Intent intent = new Intent(MainActivity.this, Choose_Login_And_Reg.class);
                 startActivity(intent);
                 finish();
             }
